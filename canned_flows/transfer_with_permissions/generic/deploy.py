7c80da24f6969fdafc0e216b160a047502b75174
Uriel Mandujano <uriel@globus.org>
2021-05-10 13:43:24 -0500

    Updates Flows to use error catching

--- canned_flows/transfer_with_permissions/generic/deploy.py
+++ canned_flows/transfer_with_permissions/generic/deploy.py
@@ -1,12 +1,15 @@
 import yaml
+from canned_flows.utils import format_and_print
 from globus_automate_client import create_flows_client
 
-from canned_flows.utils import format_and_print
-
-with open("canned_flows/move/mv.yml") as flow_def_file:
+with open(
+    "canned_flows/transfer_with_permissions/generic/definition.yml"
+) as flow_def_file:
     flow_def = yaml.safe_load(flow_def_file)
 
-with open("canned_flows/move/schema.yml") as flow_schema_file:
+with open(
+    "canned_flows/transfer_with_permissions/generic/schema.yml"
+) as flow_schema_file:
     flow_schema = yaml.safe_load(flow_schema_file)
 
 
@@ -22,5 +25,4 @@ resp = fc.deploy_flow(
     administered_by=[],
     input_schema=flow_schema,
 )
-
 format_and_print(resp)
--- canned_flows/transfer_with_permissions/generic/run.py
+++ canned_flows/transfer_with_permissions/generic/run.py
@@ -1,19 +1,20 @@
 import yaml
+from canned_flows.utils import format_and_print
 from globus_automate_client import create_flows_client
 
-from canned_flows.utils import format_and_print
-
-with open("canned_flows/move/input.yml") as flow_input_file:
+with open(
+    "canned_flows/transfer_with_permissions/generic/sample-input.yml"
+) as flow_input_file:
     flow_input = yaml.safe_load(flow_input_file)
 
 fc = create_flows_client()
 resp = fc.run_flow(
-    "e40b6cec-15eb-4d87-b4f3-8e6b0b59f65a",
+    "",
     flow_scope=None,
     flow_input=flow_input,
     monitor_by=[],
     manage_by=[],
-    label="A Run of the Flow e40b6cec-15eb-4d87-b4f3-8e6b0b59f65a",
+    label="A Run of the Flow ",
 )
 
 format_and_print(resp)
